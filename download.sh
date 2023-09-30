b84c19e5368d7d6d889f91542de3c8b93746f286
muji <muji@tmpfs.org>
2023-09-30 17:18:25 +0800

    Add size of bloom filter to file.

--- download.sh
+++ download.sh
@@ -5,3 +5,4 @@
 easypwned_haveibeenpwned_downloader --sink-bloom-file=database/hibp.bloom --sink-stdout > database/hibp.txt
 
 date -Idate > database/last-updated.txt
+wc -c < database/hibp.bloom | tr -d ' ' > database/size.txt
