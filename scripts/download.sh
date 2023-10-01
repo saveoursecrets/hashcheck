20cecd10089bf93f5abec6019c87dfefa1116d52
muji <muji@tmpfs.org>
2023-10-01 09:28:47 +0800

    Only create bloom file on download.

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,7 +2,7 @@
 
 (cd easypwned/downloader && cargo install --path .)
 
-easypwned_haveibeenpwned_downloader --sink-bloom-file=database/hibp.bloom --sink-stdout > database/hibp.txt
+easypwned_haveibeenpwned_downloader --sink-bloom-file=database/hibp.bloom
 
 date -Idate > database/last-updated.txt
 wc -c < database/hibp.bloom | tr -d ' ' > database/size.txt
