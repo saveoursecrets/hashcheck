e8a3b1145db75a1c592212ecde4822ad828a1e7c
muji <muji@tmpfs.org>
2023-10-15 11:36:16 +0800

    Tweak test script to check new deployment.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl -vXPOST http://localhost:3342/ -H 'Content-Type: application/json' --data-binary @./fixtures/hashes.json
+#curl -vXPOST http://localhost:3342/ -H 'Content-Type: application/json' --data-binary @./fixtures/hashes.json
+
+curl -vXPOST https://hashcheck.saveoursecrets.com -H 'Content-Type: application/json' --data-binary @./fixtures/hashes.json
